37521bb0f0eabe08743b0181ddba25256e80ed11
HassenHichri <hassen.hichri@outlook.com>
2017-04-16 14:27:17 +0100

    angularJS week4 ex4: Error handling in Client-Server Communication using

--- AngularJS/conFusion/app/scripts/controllers.js
+++ AngularJS/conFusion/app/scripts/controllers.js
@@ -7,9 +7,16 @@ angular.module('confusionApp')
         $scope.tab = 1;
         $scope.filtText = '';
         $scope.showDetails = false;
-        $scope.showMenu = true;
+        $scope.showMenu = false;
         $scope.message = "Loading ...";
-        $scope.dishes = menuFactory.getDishes().query();
+        menuFactory.getDishes().query(
+            function(response) {
+                $scope.dishes = response;
+                $scope.showMenu = true;
+            },
+            function(response) {
+                $scope.message = "Error: "+response.status + " " + response.statusText;
+            });
         $scope.select = function(setTab) {
             $scope.tab = setTab;
 
@@ -71,25 +78,31 @@ angular.module('confusionApp')
 
         $scope.dish = {};
         $scope.message="Loading ...";
-        $scope.showDish = true;
+        $scope.showDish = false;
         $scope.message="Loading ...";
-        $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)});
-
+        $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+            .$promise.then(
+                function(response){
+                    $scope.dish = response;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
     }])
 
-    .controller('DishCommentController', ['$scope', function($scope) {
+    .controller('DishCommentController', ['$scope','menuFactory', function($scope,menuFactory) {
 
         $scope.mycomment = {rating:5, comment:"", author:"", date:""};
 
         $scope.submitComment = function () {
-
             $scope.mycomment.date = new Date().toISOString();
             console.log($scope.mycomment);
-
             $scope.dish.comments.push($scope.mycomment);
 
+            menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
             $scope.commentForm.$setPristine();
-
             $scope.mycomment = {rating:5, comment:"", author:"", date:""};
         }
     }])
@@ -98,9 +111,18 @@ angular.module('confusionApp')
     .controller('IndexController', ['$scope', 'menuFactory','corporateFactory', function($scope, menuFactory,corporateFactory) {
 
         $scope.dish = {};
-        $scope.showDish = true;
+        $scope.showDish = false;
         $scope.message="Loading ...";
-        $scope.dish = menuFactory.getDishes().get({id:0});
+        $scope.dish = menuFactory.getDishes().get({id:0})
+            .$promise.then(
+                function(response){
+                    $scope.dish = response;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
         var promotion = menuFactory.getPromotion(0);
 
         $scope.promotion = promotion;
